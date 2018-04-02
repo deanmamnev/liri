1ee9e42f6026d731c4f400465034f929784b6b9c
Dean Mamnev <deanmamnev@gmail.com>
2018-04-02 16:47:13 -0500

    still don't know what I'm doing, big surprise

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
-require("dotenv").config();
+var dotEnv = require("dotenv").config();
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var request = require("request");
 
-
+var keys = require("./keys.js");
 
 
 
@@ -8,7 +12,18 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
-client.get('my-tweets', function(error, tweets, response) {
-    if(error) throw error;
-    console.log(tweets);  // The favorites. 
-});
+// client.get('my-tweets', function(error, tweets, response) {
+//     if(error) throw error;
+//     console.log(tweets);  // The favorites. 
+//     console.log(response);
+// });
+
+ 
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
