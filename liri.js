722f73ad0911cb8e61490e96366a3c757a29eb23
Dean Mamnev <deanmamnev@gmail.com>
2018-04-03 09:33:06 -0500

    last minute

--- liri.js
+++ liri.js
@@ -51,16 +51,16 @@ request(queryUrl, function(error, response, body) {
   // If the request is successful
   if (!error && response.statusCode === 200) {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    // console.log("Title: " + JSON.parse(body.Title));
+
+
+    console.log("Title: " + JSON.parse(body.Title));
     console.log("Release Year: " + JSON.parse(body.Year));
-    // console.log("IMDB Rating: " + JSON.parse(body.Rating));
-    // console.log("Rotton Tomatoes Rating: " + JSON.parse(body.Title));
-    // console.log("Country: " + JSON.parse(body.Country));
-    // console.log("Language: " + JSON.parse(body.Language));
-    // console.log("Plot: " + JSON.parse(body.Plot));
-    // console.log("Starring: " + JSON.parse(body.Actors));
+    console.log("IMDB Rating: " + JSON.parse(body.Rating));
+    console.log("Rotton Tomatoes Rating: " + JSON.parse(body.Title));
+    console.log("Country: " + JSON.parse(body.Country));
+    console.log("Language: " + JSON.parse(body.Language));
+    console.log("Plot: " + JSON.parse(body.Plot));
+    console.log("Starring: " + JSON.parse(body.Actors));
   }
 });
 };
